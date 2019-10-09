b82b245a918b2a5b517a06f609676d95f7f65218
cfordyce3 <40444073+cfordyce3@users.noreply.github.com>
2019-10-08 23:30:48 -0700

    Implemented process_month_gui
    Removed gui_creator from project, decided it's better to have separate programs for different use-cases (for now)

--- process_month.py
+++ process_month.py
@@ -4,12 +4,13 @@ from error_check import error_check, error_correct
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-def process_month (year='', month=''):
+def process_month (month='',year='',show_info=0):
     if (month==''): year=input('What year? ')
     if (month==''): month=input('What month? ')
 
-    print()
-    print('Processing data for ' + month + ', ' + str(year) + '... ', end='')
+    if (show_info==1):
+        print()
+        print('Processing data for ' + month + ', ' + str(year) + '... ', end='')
 
     files = []
 
@@ -21,19 +22,24 @@ def process_month (year='', month=''):
     for file in files:
         process_week(file[0],file[1],file[2])
 
-    print('Done.')
-    print()
+    if (show_info==1):
+        print('Done.')
+        print()
 
     # error checking
-    print('Error checking processed files.')
+    if (show_info==1):
+        print('Error checking processed files.')
     for file in files:
         if (error_check(file[0],file[1],file[2]) == True):
-            print(' ~ Errors found in \'' + str(file) + '\' Correcting... ', end = '')
+            if (show_info==1):
+                print(' ~ Errors found in \'' + str(file) + '\' Correcting... ', end = '')
             error_correct(file[0],file[1],file[2])
-            print('Done.')
+            if (show_info==1):
+                print('Done.')
 
-    print()
-    print('Data for ' + month + ', ' + str(year) + ' has been completed.')
-    print()
+    if (show_info==1):
+        print()
+        print('Data for ' + month + ', ' + str(year) + ' has been completed.')
+        print()
 
 #process_month()
---
+++ process_month_gui.py
@@ -0,0 +1,65 @@
+from process_month import process_month
+from tkinter import *
+
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 300
+
+YEARS = ['2019', '2020', '2021',
+         '2022', '2023', '2024',
+         '2025', '2026', '2027',
+         '2028', '2029', '2030']
+
+root = Tk()
+
+# window size
+pos_right = int(root.winfo_screenwidth()/2 - WINDOW_WIDTH/2)
+pos_down = int(root.winfo_screenheight()/2 - WINDOW_HEIGHT)
+root.geometry('{}x{}+{}+{}'.format(WINDOW_WIDTH,WINDOW_HEIGHT,pos_right,pos_down))
+
+# window title and icon
+root.title('One Stop Excel Processor ~ Monthly')
+root.iconbitmap('scu.ico')
+
+# general tabling
+root.grid_columnconfigure(0,weight=1)
+root.grid_columnconfigure(3,weight=1)
+root.grid_rowconfigure(2,weight=1)
+root.grid_rowconfigure(4,weight=1)
+
+# header
+Label(root,text='Select Which Month and Year to Process').grid(row=0,column=1,pady=30,columnspan=2)
+
+# month dropdown
+month = StringVar(root)
+month.set('January')
+month_menu = OptionMenu(root,month,'January', 'February', 'March',
+                                   'April', 'May', 'June',
+                                   'July', 'August', 'September',
+                                   'October', 'November', 'December').grid(row=1,column=1)
+
+# year dropdown
+year = StringVar(root)
+year.set('2019')
+year_menu = OptionMenu(root,year,'2019', '2020', '2021',
+                                 '2022', '2023', '2024',
+                                 '2025', '2026', '2027',
+                                 '2028', '2029', '2030').grid(row=1,column=2)
+
+# developer mode check
+developer_mode = IntVar(root)
+developer_mode.set(0)
+developer_mode_check = Checkbutton(root,text='Developer mode',variable=developer_mode).grid(row=2,column=1)
+
+# close button
+Button(root,text='Close',command=root.destroy).grid(row=3,column=1,sticky=W)
+
+def run_process_month (month,year,developer):
+    process_month(month,year,developer)
+    if (developer==1):
+        print('Done')
+
+# process button
+Button(root,text='Process',command=lambda:run_process_month(month.get(),year.get(),developer_mode.get())).grid(row=3,column=2,sticky=E)
+
+
+root.mainloop()
