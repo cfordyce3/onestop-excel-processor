ea2130e9adb7b2f64ad89ad8056d3d12265ca4d5
cfordyce3 <40444073+cfordyce3@users.noreply.github.com>
2019-10-09 16:42:06 -0700

    Updated cx_Freeze build setup

--- process_month_py_files/setup.py
+++ process_month_py_files/setup.py
@@ -1,14 +1,19 @@
 import sys
 from cx_Freeze import setup, Executable
 
-build_exe_options = {'packages': ['os','tkinter','openpyxl']}
+build_exe_options = {'packages': ['os','tkinter','openpyxl'], 'include_files': ['scu.ico']}
 
 base = None
 if (sys.platform == 'win32'):
     base = 'Win32GUI'
 
+exec = Executable('process_month_gui_standard.py',
+                  targetName = 'process_month.exe',
+                  base = base,
+                  icon = 'scu.ico')
+
 setup (name = 'Process Month',
-       version = '1',
+       version = '1.0',
        description = 'Process month application for One Stop',
        options = {'build_exe':build_exe_options},
-       executables = [Executable('process_month_gui_standard.py', base=base)])
+       executables = [exec])
