7d9fcfd4e6f4ac0ad4755b337740cec8cd718477
cfordyce3 <40444073+cfordyce3@users.noreply.github.com>
2019-10-20 21:20:36 -0700

    Laptop update
    Started multi-year quarterly processing

--- process_quarter_py_files/process_quarter.py
+++ process_quarter_py_files/process_quarter.py
@@ -32,11 +32,31 @@ def create_dict_of_inputs (start_week='',start_month='',start_year='',end_week='
     inputs = {}
 
     if (start_year==end_year):
-        year_list = {}
-        month_list = {}
-        for month_name,month_num in months.items():
-            day_list = []
-            if (month_num >= start_month_val and month_num <= end_month_val):
+        year_list = {} # dictionary of years (e.g. {2019:..., 2020:...})
+        month_list = {} # dictionary of months (e.g. {'January':...,'February':...}
+        for month_name,month_num in months.items(): # go through each month
+            day_list = [] # list of specific week per month (e.g. ['Week of January 1, 2019.xlsx', 'Week of January 8, 2019.xlsx'...])
+            if (month_num >= start_month_val and month_num <= end_month_val): # if the month is within the starting month and the ending month (one year only)
+                for file in os.listdir(FILE_DIR + '\\Statistics\\Master\\' + str(start_year) + '\\Weekly\\' + month_name):
+                    day = strip_to_day(file,month_name,start_year)
+                    if (month_name != start_month and month_name != end_month):
+                        day_list.append(file)
+                    elif (month_name == end_month and int(day) <= int(end_week)):
+                        day_list.append(file)
+                    elif (month_name == start_month and int(day) >= int(start_week)):
+                        day_list.append(file)
+                if (len(day_list) != 0):
+                    month_list[month_name] = sort_weeks(day_list,month_name,start_year)
+            if (len(month_list) != 0):
+                year_list[start_year] = month_list
+        inputs = year_list
+
+    else: # start_year != end_year
+        year_list = {} # dictionary of years (e.g. {2019:..., 2020:...})
+        month_list = {} # dictionary of months (e.g. {'January':...,'February':...}
+        for month_name,month_num in months.items(): # go through each month
+            day_list = [] # list of specific week per month (e.g. ['Week of January 1, 2019.xlsx', 'Week of January 8, 2019.xlsx'...])
+            if (month_num >= start_month_val and month_num <= end_month_val): # if the month is within the starting month and the ending month (one year only)
                 for file in os.listdir(FILE_DIR + '\\Statistics\\Master\\' + str(start_year) + '\\Weekly\\' + month_name):
                     day = strip_to_day(file,month_name,start_year)
                     if (month_name != start_month and month_name != end_month):
@@ -73,7 +93,7 @@ def row_thru (outwb,year,month,filename,first,show_info=False):
     for row in inws['B38':'N41']:
         for cell in row:
             if (show_info==True and row==3): print('Input: ')
-            if (show_info==True): print(cell.coordinate + ' ' + cell.value,end=' ')
+            if (show_info==True): print(cell.coordinate + ' ' + str(cell.value),end=' ')
             outws.cell(row=r,column=c,value=cell.value)
             c+=1
         r+=1
@@ -106,5 +126,5 @@ def process_quarter (season='',start_week='',start_month='',start_year='',end_we
 
 
 #create_dict_of_inputs('20','June','2019','9','September','2019',True)
-#process_quarter('Summer_test','20','June','2019','9','September','2019',False)
+process_quarter('Summer_test','20','June','2019','9','September','2019',False)
 #process_quarter('Test','5','August','2019','26','August','2019',False)
